2516105b550bb02ad318ed7ef1c18929b462a6cc
Notre Mahamat <mahamathassana237@gmail.com>
2019-02-18 11:58:30 +0100

    DAOMarketer + brouillon

--- ClassLibrary/DAO/DAOMarketer.cs
+++ ClassLibrary/DAO/DAOMarketer.cs
@@ -8,7 +8,103 @@ namespace ClassLibrary
 {
     public class DAOMarketer
     {
-        //Bonjour
-        //Bonjour
+        private isoftTankEntities db = null;
+
+        public DAOMarketer()
+        {
+            db = new isoftTankEntities();
+        }
+
+        public Marketer ajouter(Marketer u)
+        {
+            db.Marketers.Add(u);
+            db.SaveChanges();
+
+            return u;
+        }
+
+        public Marketer modifier(Marketer u)
+        {
+            //recherche de l'objet dans la bd
+            Marketer u2 = db.Marketers.First(x => x.codeMARKETER == u.codeMARKETER);
+
+            //verification de l'existence de l'objet dans la bd
+            if (u2 != null)
+            {
+                //mise a jour des modifications
+                u2.adresse = u.adresse;
+                u2.email = u.email;
+                u2.nom = u.nom;
+                u2.pays = u.pays;
+                u2.telephone = u.telephone;
+                u2.ville = u.ville;
+
+                //sauvegarde des nouvelles informations
+
+                db.SaveChanges();
+                return u;
+            }
+            else
+            {
+                u.nom = "Cet enregistrement n'existe pas dans la base de donnees.";
+                return u;
+            }
+        }
+
+        public Marketer rechercher(string code)
+        {
+            Marketer u = db.Marketers.First(x => x.codeMARKETER == code);
+
+            //verification de l'existence de l'objet dans la bd
+            if (u != null) return u;
+
+            else
+            {
+                u.nom = "Aucun enregistrement trouve.";
+                return u;
+            }
+        }
+
+        public Marketer supprimer(Marketer u)
+        {
+            Marketer u2 = rechercher(u.codeMARKETER);
+
+            //verification de l'existence de l'objet dans la bd
+            if (u2 != null)
+            {
+                db.Marketers.Remove(u2);
+                db.SaveChanges();
+                return u2;
+            }
+
+            else
+            {
+                u2.nom = "Aucun enregistrement trouve.";
+                return u2;
+            }
+        }
+
+        public IEnumerable<Marketer> rechercherParMC(Func<Marketer, bool> predicate)
+        {
+            var us = db.Marketers.Where(predicate);
+
+            return us.ToList();
+        }
+
+        public IEnumerable<Marketer> rechercherTous()
+        {
+            return db.Marketers.ToList();
+
+        }
+
+        ~DAOMarketer()
+        {
+            Dispose();
+        }
+        public void Dispose()
+        {
+            if (db != null) db.Dispose();
+        }
+
     }
 }
---
+++ brouillon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(brouillon.Startup))]
+namespace brouillon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
