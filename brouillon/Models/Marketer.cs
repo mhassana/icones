9f266db77d302764a171487e34e2b30670b132bf
Notre Mahamat <mahamathassana237@gmail.com>
2019-02-18 14:01:04 +0100

    Ajouter marketer

---
+++ brouillon/Models/Marketer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ClassLibrary;
+
+namespace brouillon.Models
+{
+    public class Marketer
+    {
+        public Marketer marketer { get; set; }
+    }
+}
