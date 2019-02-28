0cc07baf222d91469ee2a1044469c1d31dd49f6b
arsene <megaptchearsene@yahoo.fr>
2019-02-28 09:30:03 +0100

    stock

--- ClassLibrary/DAO/DAOImp/DAOMarketer.cs
+++ ClassLibrary/DAO/DAOImp/DAOMarketer.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary.DAO.IDAO;
 
 namespace ClassLibrary
 {
-    public class DAOMarketer
+    public class DAOMarketer : IDAOMarketer
     {
         private isoftTankEntities db = null;
 
